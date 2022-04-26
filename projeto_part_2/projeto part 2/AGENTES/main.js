580ed614fac46304951e749a45becb1c90a2ae30
Milena <100636013+milena579@users.noreply.github.com>
2022-04-26 15:56:29 -0300

    primeira postagem

---
+++ projeto_part_2/projeto part 2/AGENTES/main.js
@@ -0,0 +1,16 @@
+const card1 = document.querySelector(".primeiro");
+const card2 = document.querySelector(".segundo");
+const card3 = document.querySelector(".terceiro");
+
+card1.addEventListener("click", function (e) {
+  card1.classList.toggle('is-flipped' );
+  card1.get
+});
+
+card2.addEventListener("click", function (e) {
+  card2.classList.toggle('is-flipped' );
+});
+
+card3.addEventListener("click", function (e) {
+  card3.classList.toggle('is-flipped' );
+});
